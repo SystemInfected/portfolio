695922c23e647f39e544d659efa608ccd7377773
Sebastian Widin <contact@sebastianwidin.se>
2021-03-12 23:07:17 +0100

    Removed texteditor test

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,22 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from 'react-router-dom'
 
-import TextEditor from './components/TextEditor'
 import './App.scss'
 
 const App = () => {
-  const text = [
-    {
-      type: 'paragraph',
-      children: [{ text: 'A line of text in a paragraphs.' }],
-    },
-  ]
-
-  const [ text2, setText ] = useState([]);
-  const toolbar = [
-    'basics',
-    'lists',
-    'links'
-  ]
-
-  useEffect(() => {
-
-  });
-
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/portfolio/editor">Editortest</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path="/portfolio/editor">
+      <Switch>
+          <Route path="/">
             <>
-            <h1>Editortest</h1>
-            <TextEditor editorText={text} toolbar={toolbar} />
+            <h1>Test</h1>
             </>
           </Route>
         </Switch>
-      </div>
     </Router>
   )
 }

2f62813272e362f977d257302362ab2aff160ebe
Sebastian Widin <contact@sebastianwidin.se>
2021-03-14 01:31:59 +0100

    fonts

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,21 +1,18 @@
 import styled from 'styled-components'
-import { color } from '../css/variables'
+import { color, font } from '../css/variables'
 
 const Hero = () => {
 	return (
 		<HeaderContainer>
-			<PortraitIllustration />
-			<PortraitNodes />
+			<Header>
+				Hi!
+				<br />
+				My name is Sebastian, I'm a graphic designer and web developer.
+			</Header>
+			<PortraitIllustration>Illustration</PortraitIllustration>
+			<PortraitNodes>Nodes</PortraitNodes>
 		</HeaderContainer>
 	)
-}
-
-const PortraitIllustration = () => {
-	return <div>Portrait</div>
-}
-
-const PortraitNodes = () => {
-	return <div>Nodes</div>
 }
 
 export default Hero
@@ -23,8 +20,22 @@ export default Hero
 const HeaderContainer = styled.div`
 	background-color: ${color.mainColorDark};
 	width: 100vw;
-	height: 90vh;
+	height: 100vh;
 	display: flex;
 	align-items: center;
 	justify-content: center;
 `
+const Header = styled.h1`
+	position: absolute;
+	top: 14vh;
+	color: ${color.mainColorLight};
+	font-family: ${font.headingsFont};
+	font-size: clamp(2rem, 2vw, 2.8rem);
+	font-weight: 350;
+	letter-spacing: 0.015rem;
+	padding: 0 4rem;
+`
+
+const PortraitIllustration = styled.div``
+
+const PortraitNodes = styled.div``
--- src/css/variables.tsx
+++ src/css/variables.tsx
@@ -2,3 +2,8 @@ export const color = {
 	mainColorDark: '#1e1f2d',
 	mainColorLight: '#a9b0e0',
 }
+
+export const font = {
+	headingsFont: `'Oswald', 'Helvetica Neue', sans-serif`,
+	standardFont: `'Raleway', 'Helvetica Neue', sans-serif`,
+}

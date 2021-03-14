fc74d239d14a6851bc59ba9d453101940ab437a0
Sebastian Widin <contact@sebastianwidin.se>
2021-03-14 01:55:12 +0100

    Hero font

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
 	return (
 		<HeaderContainer>
 			<Header>
-				Hi!
+				<strong>Hi!</strong>
 				<br />
 				My name is Sebastian, I'm a graphic designer and web developer.
 			</Header>
@@ -30,12 +30,25 @@ const Header = styled.h1`
 	top: 14vh;
 	color: ${color.mainColorLight};
 	font-family: ${font.headingsFont};
-	font-size: clamp(2rem, 2vw, 2.8rem);
-	font-weight: 350;
-	letter-spacing: 0.015rem;
+	font-size: clamp(2.2rem, 2.5vw, 3.2rem);
+	font-weight: 100;
+	letter-spacing: 0.02rem;
 	padding: 0 4rem;
+	z-index: 80;
+	strong {
+		font-weight: 800;
+		letter-spacing: 0.1rem;
+	}
+`
+const Portrait = styled.div`
+	position: absolute;
+	bottom: 0;
+	height: 70vh;
+`
+const PortraitIllustration = styled(Portrait)`
+	margin-right: 5px;
 `
 
-const PortraitIllustration = styled.div``
-
-const PortraitNodes = styled.div``
+const PortraitNodes = styled(Portrait)`
+	margin-left: 5px;
+`

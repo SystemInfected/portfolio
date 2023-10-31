07139bc2d0e054138be7e73c7366a88391c374ca
Sebastian Widin <contact@sebastianwidin.se>
2023-11-01 00:17:21 +0100

    linting

--- src/components/Home/AnimatedEye.tsx
+++ src/components/Home/AnimatedEye.tsx
@@ -3,65 +3,65 @@ import React, { useRef, useEffect } from 'react'
 const AnimatedEye = () => {
 	const canvasRef = useRef<HTMLCanvasElement>(null)
 
-	const draw = (ctx: any, eyePos: any) => {
-		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-
-		// Eye background
-		ctx.beginPath()
-		ctx.arc(eyePos.x + 729.29, eyePos.y + 815.36, 120, 0, 2 * Math.PI)
-		ctx.fillStyle = '#E9E8E9'
-		ctx.fill()
-		ctx.closePath()
-
-		// Iris
-		ctx.beginPath()
-		ctx.arc(eyePos.x + 729.29, eyePos.y + 815.36, 35.29, 0, 2 * Math.PI)
-		ctx.fillStyle = '#6F7CDB'
-		ctx.strokeStyle = 'black'
-		ctx.strokeWidth = 3
-		ctx.fill()
-		ctx.stroke()
-		ctx.closePath()
-
-		// Pupil
-		ctx.beginPath()
-		ctx.arc(eyePos.x + 729.29, eyePos.y + 815.36, 15.16, 0, 2 * Math.PI)
-		ctx.fillStyle = 'black'
-		ctx.fill()
-		ctx.closePath()
-
-		// Highlights
-		ctx.beginPath()
-		ctx.arc(
-			eyePos.x * 0.9 + 746.77,
-			eyePos.y * 0.5 + 823.44,
-			2.44,
-			0,
-			2 * Math.PI
-		)
-		ctx.fillStyle = 'white'
-		ctx.fill()
-		ctx.closePath()
-
-		ctx.beginPath()
-		ctx.arc(
-			eyePos.x * 0.9 + 740.47,
-			eyePos.y * 0.5 + 820.27,
-			3.57,
-			0,
-			2 * Math.PI
-		)
-		ctx.fillStyle = 'white'
-		ctx.fill()
-		ctx.closePath()
-	}
-
-	let newMousePos = { y: 0, x: 0 }
+	const newMousePos = useRef({ y: 0, x: 0 })
 	const eyeMaxX = 900
 	const eyeMaxYTop = 500
 	const eyeMaxYBottom = 700
 
 	useEffect(() => {
+		const draw = (ctx: any, eyePos: any) => {
+			ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+
+			// Eye background
+			ctx.beginPath()
+			ctx.arc(eyePos.x + 729.29, eyePos.y + 815.36, 120, 0, 2 * Math.PI)
+			ctx.fillStyle = '#E9E8E9'
+			ctx.fill()
+			ctx.closePath()
+
+			// Iris
+			ctx.beginPath()
+			ctx.arc(eyePos.x + 729.29, eyePos.y + 815.36, 35.29, 0, 2 * Math.PI)
+			ctx.fillStyle = '#6F7CDB'
+			ctx.strokeStyle = 'black'
+			ctx.strokeWidth = 3
+			ctx.fill()
+			ctx.stroke()
+			ctx.closePath()
+
+			// Pupil
+			ctx.beginPath()
+			ctx.arc(eyePos.x + 729.29, eyePos.y + 815.36, 15.16, 0, 2 * Math.PI)
+			ctx.fillStyle = 'black'
+			ctx.fill()
+			ctx.closePath()
+
+			// Highlights
+			ctx.beginPath()
+			ctx.arc(
+				eyePos.x * 0.9 + 746.77,
+				eyePos.y * 0.5 + 823.44,
+				2.44,
+				0,
+				2 * Math.PI
+			)
+			ctx.fillStyle = 'white'
+			ctx.fill()
+			ctx.closePath()
+
+			ctx.beginPath()
+			ctx.arc(
+				eyePos.x * 0.9 + 740.47,
+				eyePos.y * 0.5 + 820.27,
+				3.57,
+				0,
+				2 * Math.PI
+			)
+			ctx.fillStyle = 'white'
+			ctx.fill()
+			ctx.closePath()
+		}
+
 		const canvas = canvasRef.current
 		const context = canvasRef.current?.getContext('2d')
 
@@ -71,7 +71,6 @@ const AnimatedEye = () => {
 
 			const headerContainer = document.querySelector('#headerContainer')
 			if (headerContainer) {
-				// eslint-disable-next-line @typescript-eslint/no-explicit-any
 				headerContainer.addEventListener('mousemove', (event: any) => {
 					const canvasXMultiplier =
 						canvas.width / canvas.getBoundingClientRect().width
@@ -84,14 +83,13 @@ const AnimatedEye = () => {
 						(event.y - canvas.getBoundingClientRect().y) * canvasYMultiplier -
 						815.36
 
-					newMousePos = { x: mouseX, y: mouseY }
+					newMousePos.current = { x: mouseX, y: mouseY }
 				})
 				headerContainer.addEventListener('mouseout', () => {
-					newMousePos = { x: 0, y: 0 }
+					newMousePos.current = { x: 0, y: 0 }
 				})
 				headerContainer.addEventListener(
 					'touchstart touchmove',
-					// eslint-disable-next-line @typescript-eslint/no-explicit-any
 					(event: any) => {
 						const canvasXMultiplier =
 							canvas.width / canvas.getBoundingClientRect().width
@@ -104,13 +102,13 @@ const AnimatedEye = () => {
 							(event.touches[0].clientY - canvas.getBoundingClientRect().y) *
 							canvasYMultiplier
 
-						newMousePos = { x: mouseX, y: mouseY }
+						newMousePos.current = { x: mouseX, y: mouseY }
 					}
 				)
 				headerContainer.addEventListener(
 					'touchend touchcancel touchleave',
 					() => {
-						newMousePos = { x: 0, y: 0 }
+						newMousePos.current = { x: 0, y: 0 }
 					}
 				)
 			}
@@ -123,13 +121,13 @@ const AnimatedEye = () => {
 		const updatedEyePos = { x: 0, y: 0 }
 		const eyeSpeed = 3
 		const render = () => {
-			if (newMousePos) {
-				if (mousePosCheck === newMousePos && frameCount > 0) {
+			if (newMousePos.current) {
+				if (mousePosCheck === newMousePos.current && frameCount > 0) {
 					mousePosDoubleCheck = mousePosCheck
 					frameCount = -180
 				}
 				if (
-					mousePosDoubleCheck === newMousePos &&
+					mousePosDoubleCheck === newMousePos.current &&
 					frameCount > -50 &&
 					frameCount < 0
 				) {
@@ -146,24 +144,24 @@ const AnimatedEye = () => {
 				} else {
 					frameCount++
 					if (
-						newMousePos.x > 50 &&
-						newMousePos.x < eyeMaxX &&
-						newMousePos.y < eyeMaxYBottom &&
-						newMousePos.y > -eyeMaxYTop
+						newMousePos.current.x > 50 &&
+						newMousePos.current.x < eyeMaxX &&
+						newMousePos.current.y < eyeMaxYBottom &&
+						newMousePos.current.y > -eyeMaxYTop
 					) {
 						if (updatedEyePos.x < 20) {
 							updatedEyePos.x += eyeSpeed
 						}
 					} else if (
-						newMousePos.x < -50 &&
-						newMousePos.x > -eyeMaxX &&
-						newMousePos.y < eyeMaxYBottom &&
-						newMousePos.y > -eyeMaxYTop
+						newMousePos.current.x < -50 &&
+						newMousePos.current.x > -eyeMaxX &&
+						newMousePos.current.y < eyeMaxYBottom &&
+						newMousePos.current.y > -eyeMaxYTop
 					) {
 						if (updatedEyePos.x > -20) {
 							updatedEyePos.x -= eyeSpeed
 						}
-					} else if (newMousePos.x === 0) {
+					} else if (newMousePos.current.x === 0) {
 						if (updatedEyePos.x > 3) {
 							updatedEyePos.x -= eyeSpeed
 						} else if (updatedEyePos.x < -3) {
@@ -175,24 +173,24 @@ const AnimatedEye = () => {
 						updatedEyePos.x += eyeSpeed
 					}
 					if (
-						newMousePos.y > 50 &&
-						newMousePos.y < eyeMaxYBottom &&
-						newMousePos.x < eyeMaxX &&
-						newMousePos.x > -eyeMaxX
+						newMousePos.current.y > 50 &&
+						newMousePos.current.y < eyeMaxYBottom &&
+						newMousePos.current.x < eyeMaxX &&
+						newMousePos.current.x > -eyeMaxX
 					) {
 						if (updatedEyePos.y < 20) {
 							updatedEyePos.y += eyeSpeed
 						}
 					} else if (
-						newMousePos.y < -50 &&
-						newMousePos.y > -eyeMaxYTop &&
-						newMousePos.x < eyeMaxX &&
-						newMousePos.x > -eyeMaxX
+						newMousePos.current.y < -50 &&
+						newMousePos.current.y > -eyeMaxYTop &&
+						newMousePos.current.x < eyeMaxX &&
+						newMousePos.current.x > -eyeMaxX
 					) {
 						if (updatedEyePos.y > -10) {
 							updatedEyePos.y -= eyeSpeed
 						}
-					} else if (newMousePos.y === 0) {
+					} else if (newMousePos.current.y === 0) {
 						if (updatedEyePos.y > 3) {
 							updatedEyePos.y -= eyeSpeed
 						} else if (updatedEyePos.y < -3) {
@@ -203,7 +201,7 @@ const AnimatedEye = () => {
 					} else if (updatedEyePos.y < -3) {
 						updatedEyePos.y += eyeSpeed
 					}
-					mousePosCheck = newMousePos
+					mousePosCheck = newMousePos.current
 				}
 			}
 
@@ -215,7 +213,7 @@ const AnimatedEye = () => {
 		return () => {
 			window.cancelAnimationFrame(animationFrameId)
 		}
-	}, [draw])
+	}, [])
 
 	return <canvas ref={canvasRef} />
 }
